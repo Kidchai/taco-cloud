3b3da95693793fc6a13c8a18aab40cefaed4a0cf
Kidchai <o.naibova@gmail.com>
2023-08-17 16:49:26 -0500

    Add validation for Taco, TacoOrder

--- src/main/java/com/kidchai/tacocloud/web/OrderController.java
+++ src/main/java/com/kidchai/tacocloud/web/OrderController.java
@@ -1,13 +1,15 @@
 package com.kidchai.tacocloud.web;
 
 import com.kidchai.tacocloud.TacoOrder;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -20,7 +22,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, BindingResult errors,
+                               SessionStatus sessionStatus) {
+        if (errors.hasErrors())
+            return "orderForm";
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
