49ecd451337f70a39fa61ddedbfb76246eebed15
Kidchai <o.naibova@gmail.com>
2023-08-27 19:06:41 -0500

    Update Ingredient, Taco, TacoOrder for MongoDB support

--- src/main/java/com/kidchai/tacocloud/Ingredient.java
+++ src/main/java/com/kidchai/tacocloud/Ingredient.java
@@ -1,18 +1,17 @@
 package com.kidchai.tacocloud;
 
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Table("ingredients")
+@Document(collection="ingredients")
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class Ingredient {
-    @PrimaryKey
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/com/kidchai/tacocloud/web/DesignTacoController.java
+++ src/main/java/com/kidchai/tacocloud/web/DesignTacoController.java
@@ -6,7 +6,6 @@ import com.kidchai.tacocloud.Ingredient.Type;
 import com.kidchai.tacocloud.Taco;
 import com.kidchai.tacocloud.TacoOrder;
 import com.kidchai.tacocloud.data.IngredientRepository;
-import com.kidchai.tacocloud.util.TacoUDRUtils;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,7 +60,7 @@ public class DesignTacoController {
         if (errors.hasErrors())
             return "design";
 
-        tacoOrder.addTaco(TacoUDRUtils.toTacoUDT(taco));
+        tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
     }
